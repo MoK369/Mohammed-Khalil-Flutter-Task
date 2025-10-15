52082f238a2e6a1234585503a5ad4adbb64e3774
Mohammed Khalil <klilmohammed9@gmail.com>
2025-10-15 15:47:10 +0300

    feat: Initial project structure
    Sets up the initial Flutter project structure for a new application.
    
    This commit includes the baseline configuration and required files for Android, iOS, and macOS platforms. It also establishes the basic Dart/Flutter lib structure, including placeholders for UI components like colors, themes, routing, and the home screen.
    
    Key additions:
    -   `pubspec.yaml` with initial dependencies (sqflite, flutter_bloc, provider, cached_network_image).
    -   Default `.gitignore`, `README.md`, and `analysis_options.yaml`.
    -   Platform-specific build configurations (`.gradle` for Android, `xcodeproj` for iOS/macOS).
    -   Boilerplate for `MainActivity.kt` and `AppDelegate.swift`.
    -   Basic `main.dart` entry point and a placeholder `HomeScreen` widget.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:otex_flutter_task/ui/core/l10n/app_localizations.dart';
+import 'package:otex_flutter_task/ui/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Otex App Test',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: HomeScreen(),
+    );
+  }
+}
